811c379d1816c6f1c5ceeab5b68d26409a437c31
GaskerNeal <myomyintsoe2017@gmail.com>
2023-02-23 11:14:18 +0630

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="home">
+  <h1>Home Page</h1>
+  <p>Hello {{name}}. I am {{age}} years old.</p>
+  <input type="text" v-model="name">
+  <button @click="handleClick">Click</button>
+  <button @click="age++">Increase Age</button>
+</div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+// @ is an alias to /src
+
+export default {
+  setup() {
+    let name = ref("MMS");
+    let age = ref(20);
+    let handleClick = () => {
+      name.value = "KZO"
+    }
+    return {name, age, handleClick};
+  }
+}
+</script>
